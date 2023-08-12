d5e0489bd26e4ffd2dc95669bd4fee17ff0d232c
wiviw5 <wiviw2221212@gmail.com>
2023-08-11 20:16:56 -0500

    Added a format discord timestamp function for future projects.

--- utils/utils.py
+++ utils/utils.py
@@ -41,3 +41,19 @@ async def fetchUserFromID(interaction: discord.Interaction, userinput: str) -> d
 
 def getChannelFromID(interaction: discord.Interaction, channel_id: int) -> discord.TextChannel:
     return interaction.client.get_channel(channel_id)
+
+
+"""
+Available flags:
+t: Short time (e.g 9:41 PM)
+T: Long Time (e.g. 9:41:30 PM)
+d: Short Date (e.g. 30/06/2021)
+D: Long Date (e.g. 30 June 2021)
+f (default): Short Date/Time (e.g. 30 June 2021 9:41 PM)
+F: Long Date/Time (e.g. Wednesday, June, 30, 2021 9:41 PM)
+R: Relative Time (e.g. 2 months ago, in an hour)
+"""
+
+
+def formatTimestamp(timestamp: int, flag: str):
+    return f"<t:{timestamp}:{flag}>"
