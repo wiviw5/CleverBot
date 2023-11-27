db4cb67c4d6a0dff85e868ac64e7b68563639f56
WiViW <wiviw.public@gmail.com>
2023-11-27 02:25:29 -0600

    Improvements to MC server pinger and to main.py bot execution.

--- cogs/minecraft_server.py
+++ cogs/minecraft_server.py
@@ -5,6 +5,7 @@ This requires the package: https://pypi.org/project/mcstatus/ / https://github.c
 """
 import base64
 import io
+import re
 import time
 
 import discord
@@ -48,22 +49,102 @@ class MCServer(commands.Cog, name="Minecraft Server Commands"):
                 names = ""
                 for player in status.players.sample:
                     names = names + f"{player.name} - {player.uuid}" + "\n"
-                embed.add_field(name="Players", value=names, inline=True)
+                embed.add_field(name="Players", value=names, inline=False)
 
+        filename = "server-icon.png"
         if status.favicon is not None:
-            filename = "server-icon.png"
             file = discord.File(io.BytesIO(base64.b64decode(status.favicon.removeprefix("data:image/png;base64,"))), filename=filename)
-            embed.set_thumbnail(url=f"attachment://{filename}")
-            await interaction.followup.send(embed=embed, file=file)
         else:
-            filename = "server-icon.png"
             file = discord.File(filename)
-            embed.set_thumbnail(url=f"attachment://{filename}")
-            await interaction.followup.send(embed=embed, file=file)
+        embed.set_thumbnail(url=f"attachment://{filename}")
+        await interaction.followup.send(embed=embed, file=file)
+
+    @mcserver.command(name="mcembed", description="Makes an embed with specific information.")
+    @app_commands.describe(name='Server Name')
+    @app_commands.describe(address='Server Address')
+    @app_commands.describe(port='Server Port')
+    @app_commands.describe(players='Whether to include a player embed button or not.')
+    async def serverembed(self, interaction: discord.Interaction, name: str, address: str, port: int, players: bool = False):
+        if not isOwner(interaction.user.id):
+            await interaction.response.send_message(f"This command is not for public use!", ephemeral=True, delete_after=5)
+            return
+        await interaction.response.defer(ephemeral=True)
+        status = await getServerStatus(address=address, port=port, retry=0)
+        if isinstance(status, str):
+            await interaction.followup.send(f"This server is currently unavailable.\nPlease retry again later.\n\nError ID: `{status}`")
+            return
+        await interaction.followup.send("Server found! Generating Embed.")
+        status: JavaStatusResponse
+
+        embed = discord.Embed(color=5200474, title=f"{name}", description=f"Server Address\n`{address}`")
+        embed.set_footer(text=f"{status.version.name}")
+
+        filename = "server-icon.png"
+        if status.favicon is not None:
+            file = discord.File(io.BytesIO(base64.b64decode(status.favicon.removeprefix("data:image/png;base64,"))), filename=filename)
+        else:
+            file = discord.File(filename)
+        embed.set_thumbnail(url=f"attachment://{filename}")
+
+        view = discord.ui.View(timeout=None)
+        view.add_item(MCButtonView(address=address, port=port, players=players))
+        await interaction.channel.send(embed=embed, file=file, view=view)
 
 
 async def setup(bot):
     await bot.add_cog(MCServer(bot))
+
+
+class MCButtonView(discord.ui.DynamicItem[discord.ui.Button], template=r'minecraftserver:info:\{(?P<info>[^}]*)\}'):
+    def __init__(self, address: str, port: int, players: bool) -> None:
+        if players:
+            label = "Get Player Status"
+            style = discord.ButtonStyle.success
+        else:
+            label = "Get Server Status"
+            style = discord.ButtonStyle.primary
+        self.address: str = address
+        self.players: bool = players
+        self.port: int = port
+        super().__init__(
+            discord.ui.Button(
+                label=label,
+                style=style,
+                custom_id=f"minecraftserver:info:{{{address}:{port}:{players}}}"
+            )
+        )
+
+    @classmethod
+    async def from_custom_id(cls, interaction: discord.Interaction, item: discord.ui.Button, match: re.Match[str], /):
+        info = str(match['info'])
+        information = info.split(":")
+        return cls(address=information[0], port=int(information[1]), players=information[2] == "True")
+
+    async def callback(self, interaction: discord.Interaction) -> None:
+        status = await getServerStatus(address=self.address, port=self.port, retry=0)
+        if isinstance(status, str):
+            await interaction.response.send_message(f"This server is currently unavailable.\nPlease retry again later.\n\nError ID: `{status}`", ephemeral=True)
+            return
+        status: JavaStatusResponse
+
+        embed = discord.Embed(color=5200474, title=f"Server Response in: `{round(status.latency, 2)}ms`", description=f"Online Players: `{status.players.online}`")
+        embed.add_field(name="MOTD", value=status.motd.to_plain(), inline=False)
+        embed.set_footer(text=f"{self.address} - {status.version.name}")
+
+        if self.players:
+            if status.players.sample is not None and len(status.players.sample) > 0:
+                names = ""
+                for player in status.players.sample:
+                    names = names + f"{player.name} - {player.uuid}" + "\n"
+                embed.add_field(name="Players", value=names, inline=False)
+
+        filename = "server-icon.png"
+        if status.favicon is not None:
+            file = discord.File(io.BytesIO(base64.b64decode(status.favicon.removeprefix("data:image/png;base64,"))), filename=filename)
+        else:
+            file = discord.File(filename)
+        embed.set_thumbnail(url=f"attachment://{filename}")
+        await interaction.response.send_message(embed=embed, file=file, ephemeral=True)
 
 
 async def getServerStatus(address: str, port: int, retry: int) -> [JavaStatusResponse, str]:
@@ -91,6 +172,9 @@ async def getServerStatus(address: str, port: int, retry: int) -> [JavaStatusRes
             return getErrorType(2)
         time.sleep(5)
         return await getServerStatus(address=address, port=port, retry=2)
+
+
+
     return status
 
 
--- main.py
+++ main.py
@@ -1,15 +1,34 @@
-import asyncio
 import os
 
 import discord
-from discord.ext.commands import Bot
+from discord.ext import commands
 
+from cogs.minecraft_server import MCButtonView
 from utils.bot_config import getMainGuildID
 from utils.bot_secrets import getBotToken
 from utils.utils import getTime, loadConfigs
 
-intents = discord.Intents.default()
-bot = Bot(intents=intents, command_prefix="!")
+
+class CleverBot(commands.Bot):
+    def __init__(self):
+        intents = discord.Intents.default()
+        intents.message_content = True
+
+        super().__init__(command_prefix='!', intents=intents)
+
+    async def setup_hook(self) -> None:
+        self.add_dynamic_items(MCButtonView)
+
+    async def on_ready(self):
+        await load_cogs()
+        print(f"Bot connected at: {getTime()} as {self.user} (ID: {self.user.id})")
+        commands = await self.tree.fetch_commands(guild=discord.Object(getMainGuildID()))
+        if len(commands) < 1:
+            print(f"Manually resynced due to guild with ID {getMainGuildID()} having lost all commands.")
+            await self.tree.sync(guild=discord.Object(getMainGuildID()))
+
+
+bot = CleverBot()
 
 
 async def load_cogs():
@@ -23,16 +42,6 @@ async def load_cogs():
             except Exception as e:
                 exception = f"{type(e).__name__}: {e}"
                 print(f"Failed to load extension {extension}\n{exception}")
-
-
-@bot.event
-async def on_ready():
-    await load_cogs()
-    print(f"Bot connected at: {getTime()} as {bot.user} (ID: {bot.user.id})")
-    commands = await bot.tree.fetch_commands(guild=discord.Object(getMainGuildID()))
-    if len(commands) < 1:
-        print(f"Manually resynced due to guild with ID {getMainGuildID()} having lost all commands.")
-        await bot.tree.sync(guild=discord.Object(getMainGuildID()))
 
 
 async def unsync_commands():
